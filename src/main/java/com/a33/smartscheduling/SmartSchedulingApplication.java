617edc32356a1ef66f1ee4a8f7eb301dbfd34434
hui <2083279247@qq.com>
2023-03-17 16:31:46 +0800

    empty project

---
+++ src/main/java/com/a33/smartscheduling/SmartSchedulingApplication.java
@@ -0,0 +1,13 @@
+package com.a33.smartscheduling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartSchedulingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartSchedulingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/a33/smartscheduling/SmartSchedulingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a33.smartscheduling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartSchedulingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
