791caf5a40eada1b2883af0501fcbb376efb3321
hui <2083279247@qq.com>
2023-03-31 17:14:03 +0800

    chuShi

--- src/main/java/com/a33/smartscheduling/SmartSchedulingApplication.java
+++ src/main/java/com/a33/smartscheduling/SmartSchedulingApplication.java
@@ -1,8 +1,10 @@
 package com.a33.smartscheduling;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.a33.smartscheduling.*.mapper")
 @SpringBootApplication
 public class SmartSchedulingApplication {
 
--- src/test/java/com/a33/smartscheduling/SmartSchedulingApplicationTests.java
+++ src/test/java/com/a33/smartscheduling/SmartSchedulingApplicationTests.java
@@ -1,13 +1,24 @@
 package com.a33.smartscheduling;
 
+import com.a33.smartscheduling.sys.entity.Emp;
+import com.a33.smartscheduling.sys.mapper.EmpMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 @SpringBootTest
 class SmartSchedulingApplicationTests {
 
+    @Resource
+    private EmpMapper empMapper;
+
     @Test
     void contextLoads() {
+        List<Emp> emps = empMapper.selectList(null);
+        emps.forEach(System.out::println);
     }
+
 
 }
